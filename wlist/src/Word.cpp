8b738f25701deaa559eb51cbacf234428084071e
Raphael Nestler <rnestler@hsr.ch>
2012-11-08 16:56:11 +0100

    fancy Variante für Kreativitätspunkte ;)

--- wlist/src/Word.cpp
+++ wlist/src/Word.cpp
@@ -1,7 +1,10 @@
 #include "Word.h"
 #include <cctype>
+#include <iostream>
 
 void Word::read(std::istream& in) {
+
+	/*
 	while(in.good()) {
 		char c = in.get();
 		if(::isalpha(c)) {
@@ -13,6 +16,30 @@ void Word::read(std::istream& in) {
 			}
 		}
 	}
+	*/
+
+
+	// @TODO: use temporary string and only apply to m_Word when succsessfull
+
+	using input=std::istreambuf_iterator<char>;
+	input eof{};
+	input init{in};
+	auto it=std::find_if(init, eof, [this](const char c) {
+			if(::isalpha(c)) {
+				this->m_Word.push_back(c);
+			}
+			else {
+				if(! this->m_Word.empty()) {
+					return true;
+				}
+			}
+			return false;
+		}
+	);
+	if( it==eof) {
+		in.clear(std::ios_base::eofbit | in.rdstate());
+	}
+	if (m_Word.empty()) in.clear(std::ios_base::failbit|in.rdstate());
 	/*
 	do {
 		std::string tmp;
