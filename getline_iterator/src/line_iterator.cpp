891768bcb3416a2b20d7411ef8ff87de725d0195
Raphael Nestler <rnestler@hsr.ch>
2012-11-29 00:06:05 +0100

    working version using boost::iterator_facade

--- getline_iterator/src/line_iterator.cpp
+++ getline_iterator/src/line_iterator.cpp
@@ -2,7 +2,9 @@
 
 //std::istringstream line_iterator::dummy{};
 
+/*
 bool line_iterator::operator==( const line_iterator& b) const
 {
 	return in == b.in;
 }
+*/
--- getline_iterator/src/line_iterator.h
+++ getline_iterator/src/line_iterator.h
@@ -2,41 +2,50 @@
 #define LINE_ITERATOR_H_
 
 #include <boost/operators.hpp>
-#include <sstream>
 
-class line_iterator : boost::input_iterator_helper<line_iterator, std::string>
+#include <istream>
+#include <boost/iterator/iterator_facade.hpp>
+
+class line_iterator :
+	public boost::iterator_facade<
+		line_iterator,
+		std::string,
+		std::input_iterator_tag,
+		std::string>
 
 {
+	friend class boost::iterator_core_access;
+
+    value_type dereference() const
+    {
+        return m_current_value;
+    }
+
+    bool equal(const line_iterator & rhs) const
+    {
+        return m_istream == rhs.m_istream;
+    }
+
+    void increment()
+    {
+        if(0 != m_istream){
+            getline(*m_istream, m_current_value);
+            if(! m_istream->good()){
+                m_istream = 0;
+            }
+        }
+    }
+
 public:
 	explicit line_iterator()
-		:in(0)
+		:m_istream(0)
 	{}
 	explicit line_iterator(std::istream &input)
-		:in(&input)
+		:m_istream(&input)
 	{}
-
-	value_type operator*() const
-	{
-		return value;
-	}
-
-	line_iterator& operator++()
-	{
-		if(in) {
-			getline(*in, value);
-			if( !in->good() ) {
-				in = 0;
-			}
-		}
-		return *this;
-	}
-
-	bool operator==( const line_iterator& b) const;
-
 private:
-	value_type value;
-	std::istream *in;
-	//static std::istringstream dummy;
+	value_type m_current_value;
+	std::istream *m_istream;
 };
 
 #endif /* LINE_ITERATOR_H_ */
