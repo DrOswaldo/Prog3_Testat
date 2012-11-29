0ade7c58166e8a957af2a647f0cc58d56e544547
Raphael Nestler <rnestler@hsr.ch>
2012-11-29 16:31:04 +0100

    added constructor test

--- dynArray/src/Test.cpp
+++ dynArray/src/Test.cpp
@@ -2,14 +2,28 @@
 #include "ide_listener.h"
 #include "cute_runner.h"
 
-void thisIsATest() {
+#include "dynArray.h"
+
+template<class T>
+dynArray<T> makedynArray(std::initializer_list<T> ilist)
+{
+	return dynArray<T>{ilist};
+}
+
+void constructorTest() {
+	auto dCharArray = makedynArray<char>({});
+
+
+	dynArray<int> dIntArray{1,2,3,4};
+	dynArray<double> dDoubleArray(10,3.14);
+	dynArray<std::string> dStringArray(std::istream_iterator<std::string>{std::cin},std::istream_iterator<std::string>{});
+
 	ASSERTM("start writing tests", false);	
 }
 
 void runSuite(){
 	cute::suite s;
-	//TODO add your test here
-	s.push_back(CUTE(thisIsATest));
+	s.push_back(CUTE(constructorTest));
 	cute::ide_listener lis;
 	cute::makeRunner(lis)(s, "The Suite");
 }
