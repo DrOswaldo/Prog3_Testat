a8dcb935353e8d987a40145d7c206c5d0ea5b20f
gutzwillerbeat <bgutzwil@hsr.ch>
2012-12-13 00:49:58 +0100

    blah

--- dynArray/src/dynArray.h
+++ dynArray/src/dynArray.h
@@ -4,12 +4,33 @@
 template<class T>
 class dynArray
 {
+	template< typename T > class array {
+	private:
+		int size;
+		T *myarray;
+	public:
+	// constructor with user pre-defined size
+		array (int s) {
+		size = s;
+		myarray = new T [size];
+		}
+	// calss array member function to set element of myarray
+	// with type T values
+	void setArray ( int elem, T val) {
+		myarray[elem] = val;
+		}
 
-public:
-	operator
-private:
-	std::vector<T> m_vector;
-};
+	// for loop to display all elements of an array
+	void getArray () {
+	     for ( int j = 0; j < size; j++ ) {
+	// typeid will retriev a type for each value
+	        cout << setw( 7 ) << j << setw( 13 ) << myarray[ j ]
+	<< " type: " << typeid(myarray[ j ]).name() << endl;
+		}
+		cout << "-----------------------------" << endl;
+		}
+	};
+}
 
 
 #endif // DYNARRAY_H_
